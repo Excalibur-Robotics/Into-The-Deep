a2ea45f4ee3f1fa643517eed30fe98c5bb3637b8
Vishwanth-B <vishwanthbolisetty@gmail.com>
2024-10-12 14:23:50 -0400

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="TeleOpBasic", group="Linear OpMode")
@@ -21,6 +23,8 @@ public class BasicTeleOp extends LinearOpMode {
     private DcMotor backRight = null;
     private DcMotorEx slideL = null;
     private DcMotorEx slideR = null;
+    private Servo rotationalServo = null;
+    private CRServo intakeServo = null;
 
 
     @Override
@@ -42,10 +46,12 @@ public class BasicTeleOp extends LinearOpMode {
         slideL = hardwareMap.get(DcMotorEx.class, "SlideL");
         slideR = hardwareMap.get(DcMotorEx.class, "SlideR");
 
+        // intake servos
+
+        rotationalServo = hardwareMap.get(Servo.class, "intakePivot");
+        intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
 
 
-//        // Plane Launcher
-//        // Servos
 
 
 
@@ -74,6 +80,9 @@ public class BasicTeleOp extends LinearOpMode {
 
 
 
+
+
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
@@ -85,6 +94,7 @@ public class BasicTeleOp extends LinearOpMode {
         slideR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         slideL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         slideR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
 
 
         // run until the end of the match (driver presses STOP)
@@ -106,6 +116,7 @@ public class BasicTeleOp extends LinearOpMode {
 
 
 
+
             if (slideR.getCurrentPosition() > 10 && slideL.getCurrentPosition() > 10) {
                 slideR.setPower(slidesDefaultPower);
                 slideL.setPower(slidesDefaultPower);
@@ -116,7 +127,7 @@ public class BasicTeleOp extends LinearOpMode {
             double x = gamepad1.left_stick_x;
             double y = -gamepad1.left_stick_y;
             double turn = gamepad1.right_trigger - gamepad1.left_trigger;
-            double intakePower = -gamepad2.left_stick_y;
+            double intakePower = -gamepad2.right_stick_y;
 
             double theta = Math.atan2(y, x);
             double power = Math.hypot(x, y);
@@ -138,6 +149,18 @@ public class BasicTeleOp extends LinearOpMode {
                 rightRearPower /= power + turn;
             }
 
+            //mate intake liftoff
+            //TODO: Set servo position
+            if (gamepad1.right_bumper){
+                rotationalServo.setPosition(0.2);
+            }
+
+            if (gamepad1.left_bumper){
+                rotationalServo.setPosition(0);
+            }
+
+
+
 
             // For the Slide(s)
             // TODO: make sure autos bring slides down at the end
@@ -150,6 +173,7 @@ public class BasicTeleOp extends LinearOpMode {
                 slideHeight = 0;
                 yCounter =0;
             }
+
 
 
             // change positions when y is pressed
@@ -188,7 +212,7 @@ public class BasicTeleOp extends LinearOpMode {
             slideL.setPower(-1);
             slideR.setPower(1);
 
-
+            intakeServo.setPower(intakePower);
 
 
             // Show the elapsed game time and wheel power.
